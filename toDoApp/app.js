0afcc18220e3cdb117879023b23933ab79e5e952
Vladimir Solovyov <leilyngale@icloud.com>
2022-08-29 12:36:22 +0300

    Added basic EJS template

--- toDoApp/app.js
+++ toDoApp/app.js
@@ -2,10 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.send('hi');
+    var today = new Date().getDay();
+    var day = '';
+
+    switch(today){
+        case 0:
+            day = 'Sunday';
+            break;
+        case 1:
+            day = 'Monday';
+            break;
+        case 2:
+            day = 'Tuesday';
+            break;
+        case 3:
+            day = 'Wednesday';
+            break;
+        case 4:
+            day = 'Thursday';
+            break;
+        case 5:
+            day = 'Friday';
+            break;
+        case 6:
+            day = 'Saturday';
+            break;
+        default:
+            day = "unknown day";
+            break;
+    }
+    res.render('list', {kindOfDay: day});
 });
 
 
