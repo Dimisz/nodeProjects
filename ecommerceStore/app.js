f72cd2654181267edc2d7667ca965b0d01e2b703
Vladimir Solovyov <leilyngale@icloud.com>
2022-08-29 18:12:43 +0300

    implemented initial get and post requests for add product page

--- ecommerceStore/app.js
+++ ecommerceStore/app.js
@@ -1,17 +1,22 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Mello from Express!</h1>');
 });
 
 const server = http.createServer(app);
