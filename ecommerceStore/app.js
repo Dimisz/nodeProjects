c294cdfa179704a426d67d9bbc51ce1b0c490fff
Vladimir Solovyov <leilyngale@icloud.com>
2022-08-30 08:53:20 +0300

    added routes folder to give the project a structure

--- ecommerceStore/app.js
+++ ecommerceStore/app.js
@@ -1,24 +1,15 @@
-const http = require('http');
-
 const express = require('express');
 const bodyParser = require('body-parser');
+
+// importing my routes 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Mello from Express!</h1>');
-});
-
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
